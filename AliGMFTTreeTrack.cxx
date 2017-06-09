101077b7c5b55126a6c766dc1465d71e2ee9b655
rbertens <rbertens@cern.ch>
2017-06-10 01:28:02 +0200

    minor cleanup

--- AliGMFTTreeTrack.cxx
+++ AliGMFTTreeTrack.cxx
@@ -1,23 +1,4 @@
-/**************************************************************************
- * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
- *                                                                        *
- * Author: The ALICE Off-line Project.                                    *
- * Contributors are mentioned in the code where appropriate.              *
- *                                                                        *
- * Permission to use, copy, modify and distribute this software and its   *
- * documentation strictly for non-commercial purposes is hereby granted   *
- * without fee, provided that the above copyright notice appears in all   *
- * copies and that both the copyright notice and this permission notice   *
- * appear in the supporting documentation. The authors make no claims     *
- * about the suitability of this software for any purpose. It is          *
- * provided "as is" without express or implied warranty.                  *
- **************************************************************************/
-
-/* example track class which can stored ina ttree
- * see macros in PWGCF/FLOW/Documentation/examples/manual/ttree/macros for
- * usage info
- * author: redmer alexander bertens (rbertnens@cern.ch)
- */
+// author: redmer alexander bertens (rbertnens@cern.ch)
 
 #include "AliGMFTTreeTrack.h"
 
