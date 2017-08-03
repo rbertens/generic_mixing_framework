e049b65145c105d6ff30714d1e8895e65f9b577d
rbertens <rbertens@cern.ch>
2017-08-03 09:26:35 -0400

    add qsub launcher scripts

---
+++ qsub_create_mixed_events.sh
@@ -0,0 +1,69 @@
+clear
+echo " -------------------------------- "
+echo "    welcome to the event mixer "
+echo "--------------------------------- "
+echo " "
+if [ $# -lt 8 ]; then
+  echo 1>&2 "FATAL - not enough arguments given"
+  echo " " 
+  echo " -  please provide 8 arguments: "
+  echo "   > multiplicity range [1 - 2]" 
+  echo "   > vertex range [3 - 4]"
+  echo "   > event plane range [5 - 6]"
+  echo "   > centrality range [7 - 8]"
+  echo "------------------------------- " 
+  exit 2
+elif [ $# -gt 8 ]; then
+  echo 1>&2 "FATAL - too many arguments"
+  echo " " 
+  echo " -  please provide 8 arguments: "
+  echo "   > multiplicity range [1 - 2]" 
+  echo "   > vertex range [3 - 4]"
+  echo "   > event plane range [5 - 6]"
+  echo "   > centrality range [7 - 8]"
+  echo "------------------------------- " 
+  exit 2
+fi
+
+
+echo " - configuration: "
+echo "   > multiplicity range [$1 - $2]" 
+echo "   > vertex range [$3 - $4]"
+echo "   > event plane range [$5 - $6]"
+echo "   > centrality range [$7 - $8]"
+echo "  "
+echo "  - I wish you a happy mixing -"
+
+
+# create a unique folder
+mkdir m_$1_$2_$3_$4_$5_$6_$7_$8
+cd m_$1_$2_$3_$4_$5_$6_$7_$8
+
+
+# prepare running through bash, priority, name in the queue
+echo "#!/bin/bash" >> create_mixed_events_autoscript.sh    
+echo "#$ -N GMF_$1_$2_$3_$4_$5_$6_$7_$8" >> create_mixed_events_autoscript.sh    
+echo "#$ -q medium*" >> create_mixed_events_autoscript.sh    
+echo "#$ -S /bin/bash" >> create_mixed_events_autoscript.sh    
+echo "#$ -cwd" >> create_mixed_events_autoscript.sh    
+
+# set some specific paths. ugly, but no other way
+echo "export rd=/nics/c/home/rbertens" >> create_mixed_events_autoscript.sh
+echo "export ROOTSYS=/nics/c/home/rbertens/root/mybuild" >> create_mixed_events_autoscript.sh
+echo "export PATH=$PATH:$ROOTSYS/bin " >> create_mixed_events_autoscript.sh
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib " >> create_mixed_events_autoscript.sh
+echo "export FASTJET=/nics/c/home/rbertens/fastjet/fastjet-3.2.0/build" >> create_mixed_events_autoscript.sh
+echo "export LD_LIBRARY_PATH=$FASTJET/lib\:$LD_LIBRARY_PATH" >> create_mixed_events_autoscript.sh
+
+export PATH_TO_SOURCE=/nics/c/home/rbertens/projects/generic_mixing_framework
+echo "export PATH_TO_SOURCE=$PATH_TO_SOURCE" >> create_mixed_events_autoscript.sh
+echo "export PATH_TO_DATA=/nics/c/home/rbertens/projects/generic_mixing_framework/merge" >> create_mixed_events_autoscript.sh
+
+echo "root -q -b '$PATH_TO_SOURCE/runEventMixer.C($1,$2,$3,$4,$5,$6,$7,$8)'" >> create_mixed_events_autoscript.sh
+
+# change permissions
+chmod +x create_mixed_events_autoscript.sh
+
+# launch the autolauncher
+qsub create_mixed_events_autoscript.sh
+cd ..    
--- runEventMixer.C
+++ runEventMixer.C
@@ -1,104 +1,117 @@
-void runEventMixer()
+void runEventMixer(
+        Int_t minMult = 200,
+        Int_t maxMult = 600,
+        Float_t minVtx = -10,
+        Float_t maxVtx = 10,
+        Float_t minEp = -10,
+        Float_t maxEp = 10,
+        Float_t minCen = 40,
+        Float_t maxCen = 50
+        )
 {
 
     // example macro to read data from a ttree and perform simple analysis
     // author: Redmer Alexander Bertens (rbertens@cern.ch)
 
+
+    // $PATH_TO_SOURCE can refer to the path to the source
+    // if left empty, it's a harmless qualifier
+
     // include paths, necessary for compilation
-    gSystem->AddIncludePath("-Wno-deprecated");
- 
-    // compile the encapsulated classes
-    gROOT->LoadMacro("AliGMFTTreeHeader.cxx+");
-    gROOT->LoadMacro("AliGMFTTreeTrack.cxx+");
-    gROOT->LoadMacro("AliGMFEventContainer.cxx+");
-    gROOT->LoadMacro("AliGMFEventReader.cxx+");
-    gROOT->LoadMacro("AliGMFHistogramManager.cxx+");
-    gROOT->LoadMacro("AliGMFMixingManager.cxx+");
+    gSystem->AddIncludePath("-Wno-deprecated -I$PATH_TO_SOURCE");
+
+    // compile the classes
+    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFTTreeHeader.cxx+");
+    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFTTreeTrack.cxx+");
+    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFEventContainer.cxx+");
+    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFEventReader.cxx+");
+    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFHistogramManager.cxx+");
+    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFMixingManager.cxx+");
 
     // define the input chain and create an event reader
     TChain* myChain = new TChain("tree");
-    myChain->Add("merge/000137161.root");
-    myChain->Add("merge/000137162.root");
-    myChain->Add("merge/000137231.root");
-    myChain->Add("merge/000137232.root");
-    myChain->Add("merge/000137235.root");
-    myChain->Add("merge/000137236.root");
-    myChain->Add("merge/000137243.root");
-    myChain->Add("merge/000137430.root");
-    myChain->Add("merge/000137431.root");
-    myChain->Add("merge/000137432.root");
-    myChain->Add("merge/000137434.root");
-    myChain->Add("merge/000137439.root");
-    myChain->Add("merge/000137440.root");
-    myChain->Add("merge/000137441.root");
-    myChain->Add("merge/000137443.root");
-    myChain->Add("merge/000137530.root");
-    myChain->Add("merge/000137531.root");
-    myChain->Add("merge/000137539.root");
-    myChain->Add("merge/000137541.root");
-    myChain->Add("merge/000137544.root");
-    myChain->Add("merge/000137549.root");
-    myChain->Add("merge/000137595.root");
-    myChain->Add("merge/000137608.root");
-    myChain->Add("merge/000137638.root");
-    myChain->Add("merge/000137639.root");
-    myChain->Add("merge/000137685.root");
-    myChain->Add("merge/000137686.root");
-    myChain->Add("merge/000137691.root");
-    myChain->Add("merge/000137692.root");
-    myChain->Add("merge/000137693.root");
-    myChain->Add("merge/000137704.root");
-    myChain->Add("merge/000137718.root");
-    myChain->Add("merge/000137722.root");
-    myChain->Add("merge/000137724.root");
-    myChain->Add("merge/000137751.root");
-    myChain->Add("merge/000137752.root");
-    myChain->Add("merge/000137844.root");
-    myChain->Add("merge/000137848.root");
-    myChain->Add("merge/000138190.root");
-    myChain->Add("merge/000138192.root");
-    myChain->Add("merge/000138197.root");
-    myChain->Add("merge/000138201.root");
-    myChain->Add("merge/000138225.root");
-//    myChain->Add("merge/000138275.root");
-    myChain->Add("merge/000138364.root");
-    myChain->Add("merge/000138439.root");
-    myChain->Add("merge/000138442.root");
-    myChain->Add("merge/000138469.root");
-//    myChain->Add("merge/000138534.root");
-    myChain->Add("merge/000138578.root");
-    myChain->Add("merge/000138579.root");
-    myChain->Add("merge/000138582.root");
-    myChain->Add("merge/000138583.root");
-    myChain->Add("merge/000138621.root");
-    myChain->Add("merge/000138624.root");
-    myChain->Add("merge/000138638.root");
-    myChain->Add("merge/000138652.root");
-    myChain->Add("merge/000138653.root");
-    myChain->Add("merge/000138662.root");
-    myChain->Add("merge/000138837.root");
-    myChain->Add("merge/000138870.root");
-    myChain->Add("merge/000138871.root");
-    myChain->Add("merge/000138872.root");
-    myChain->Add("merge/000139028.root");
-    myChain->Add("merge/000139029.root");
-    myChain->Add("merge/000139036.root");
-    myChain->Add("merge/000139037.root");
-//    myChain->Add("merge/000139038.root");
-    myChain->Add("merge/000139105.root");
-    myChain->Add("merge/000139107.root");
-    myChain->Add("merge/000139173.root");
-    myChain->Add("merge/000139309.root");
-    myChain->Add("merge/000139310.root");
-    myChain->Add("merge/000139314.root");
-    myChain->Add("merge/000139328.root");
-    myChain->Add("merge/000139329.root");
-//    myChain->Add("merge/000139437.root");
-    myChain->Add("merge/000139438.root");
-    myChain->Add("merge/000139503.root");
-    myChain->Add("merge/000139505.root");
-    myChain->Add("merge/000139507.root");
-    myChain->Add("merge/000139510.root");
+    myChain->Add("$PATH_TO_DATA/000137161.root");
+    myChain->Add("$PATH_TO_DATA/000137162.root");
+    myChain->Add("$PATH_TO_DATA/000137231.root");
+    myChain->Add("$PATH_TO_DATA/000137232.root");
+    myChain->Add("$PATH_TO_DATA/000137235.root");
+    myChain->Add("$PATH_TO_DATA/000137236.root");
+    myChain->Add("$PATH_TO_DATA/000137243.root");
+    myChain->Add("$PATH_TO_DATA/000137430.root");
+    myChain->Add("$PATH_TO_DATA/000137431.root");
+    myChain->Add("$PATH_TO_DATA/000137432.root");
+    myChain->Add("$PATH_TO_DATA/000137434.root");
+    myChain->Add("$PATH_TO_DATA/000137439.root");
+    myChain->Add("$PATH_TO_DATA/000137440.root");
+    myChain->Add("$PATH_TO_DATA/000137441.root");
+    myChain->Add("$PATH_TO_DATA/000137443.root");
+    myChain->Add("$PATH_TO_DATA/000137530.root");
+    myChain->Add("$PATH_TO_DATA/000137531.root");
+    myChain->Add("$PATH_TO_DATA/000137539.root");
+    myChain->Add("$PATH_TO_DATA/000137541.root");
+    myChain->Add("$PATH_TO_DATA/000137544.root");
+    myChain->Add("$PATH_TO_DATA/000137549.root");
+    myChain->Add("$PATH_TO_DATA/000137595.root");
+    myChain->Add("$PATH_TO_DATA/000137608.root");
+    myChain->Add("$PATH_TO_DATA/000137638.root");
+    myChain->Add("$PATH_TO_DATA/000137639.root");
+    myChain->Add("$PATH_TO_DATA/000137685.root");
+    myChain->Add("$PATH_TO_DATA/000137686.root");
+    myChain->Add("$PATH_TO_DATA/000137691.root");
+    myChain->Add("$PATH_TO_DATA/000137692.root");
+    myChain->Add("$PATH_TO_DATA/000137693.root");
+    myChain->Add("$PATH_TO_DATA/000137704.root");
+    myChain->Add("$PATH_TO_DATA/000137718.root");
+    myChain->Add("$PATH_TO_DATA/000137722.root");
+    myChain->Add("$PATH_TO_DATA/000137724.root");
+    myChain->Add("$PATH_TO_DATA/000137751.root");
+    myChain->Add("$PATH_TO_DATA/000137752.root");
+    myChain->Add("$PATH_TO_DATA/000137844.root");
+    myChain->Add("$PATH_TO_DATA/000137848.root");
+    myChain->Add("$PATH_TO_DATA/000138190.root");
+    myChain->Add("$PATH_TO_DATA/000138192.root");
+    myChain->Add("$PATH_TO_DATA/000138197.root");
+    myChain->Add("$PATH_TO_DATA/000138201.root");
+    myChain->Add("$PATH_TO_DATA/000138225.root");
+//    myChain->Add("$PATH_TO_DATA/000138275.root");
+    myChain->Add("$PATH_TO_DATA/000138364.root");
+    myChain->Add("$PATH_TO_DATA/000138439.root");
+    myChain->Add("$PATH_TO_DATA/000138442.root");
+    myChain->Add("$PATH_TO_DATA/000138469.root");
+//    myChain->Add("$PATH_TO_DATA/000138534.root");
+    myChain->Add("$PATH_TO_DATA/000138578.root");
+    myChain->Add("$PATH_TO_DATA/000138579.root");
+    myChain->Add("$PATH_TO_DATA/000138582.root");
+    myChain->Add("$PATH_TO_DATA/000138583.root");
+    myChain->Add("$PATH_TO_DATA/000138621.root");
+    myChain->Add("$PATH_TO_DATA/000138624.root");
+    myChain->Add("$PATH_TO_DATA/000138638.root");
+    myChain->Add("$PATH_TO_DATA/000138652.root");
+    myChain->Add("$PATH_TO_DATA/000138653.root");
+    myChain->Add("$PATH_TO_DATA/000138662.root");
+    myChain->Add("$PATH_TO_DATA/000138837.root");
+    myChain->Add("$PATH_TO_DATA/000138870.root");
+    myChain->Add("$PATH_TO_DATA/000138871.root");
+    myChain->Add("$PATH_TO_DATA/000138872.root");
+    myChain->Add("$PATH_TO_DATA/000139028.root");
+    myChain->Add("$PATH_TO_DATA/000139029.root");
+    myChain->Add("$PATH_TO_DATA/000139036.root");
+    myChain->Add("$PATH_TO_DATA/000139037.root");
+//    myChain->Add("$PATH_TO_DATA/000139038.root");
+    myChain->Add("$PATH_TO_DATA/000139105.root");
+    myChain->Add("$PATH_TO_DATA/000139107.root");
+    myChain->Add("$PATH_TO_DATA/000139173.root");
+    myChain->Add("$PATH_TO_DATA/000139309.root");
+    myChain->Add("$PATH_TO_DATA/000139310.root");
+    myChain->Add("$PATH_TO_DATA/000139314.root");
+    myChain->Add("$PATH_TO_DATA/000139328.root");
+    myChain->Add("$PATH_TO_DATA/000139329.root");
+//    myChain->Add("$PATH_TO_DATA/000139437.root");
+    myChain->Add("$PATH_TO_DATA/000139438.root");
+    myChain->Add("$PATH_TO_DATA/000139503.root");
+    myChain->Add("$PATH_TO_DATA/000139505.root");
+    myChain->Add("$PATH_TO_DATA/000139507.root");
+    myChain->Add("$PATH_TO_DATA/000139510.root");
 
     AliGMFEventReader* reader = new AliGMFEventReader(myChain);
     // create the mixer and connect the input event reader
@@ -111,6 +124,7 @@ void runEventMixer()
     mixer->SetEventPlaneRange(-10, 10);
     mixer->SetCentralityRange(40, 50);
     mixer->SetMaxEventsPerFile(100000);
+    mixer->SetMaxEvents(100);
     mixer->DoQA();
 
     // run the mixer
