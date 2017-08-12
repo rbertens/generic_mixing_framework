519e444fcfd18c6516c2d541a681db5ff9b6dce2
rbertens <rbertens@cern.ch>
2017-08-12 19:29:21 +0200

    realstic kickstart

--- kickstart_qsub.sh
+++ kickstart_qsub.sh
@@ -7,14 +7,14 @@
 cen=(0 10)
 
 # vertex arrays
-vertex=( -10 -8 -6 -4 -2 0  2 4 5 6 8 10)
+vertex=(-10 -6 -2 2 6 10)
 
 # event plane array
 ep=(-1.6 -.785 0, .785 1.6)
 
 # multiplicity array
 
-mult=(1000 1500 2000 2500 3000)
+mult=(1280 1720 2000 2160 2600)
 
 for ((i = 0; i < ${#vertex[@]}-1; ++i)); do
     inci=$(( $i + 1 ))
@@ -22,7 +22,7 @@ for ((i = 0; i < ${#vertex[@]}-1; ++i)); do
         incj=$(( $j + 1 ))
         for ((k = 0; k < ${#mult[@]}-1; ++k)); do
             inck=$(( $k + 1 ))
-            echo "qsub_create_mixed_events.sh ${mult[$k]} ${mult[$inck]} ${vertex[$i]} ${vertex[$inci]} ${ep[$j]} ${ep[$incj]} ${cen[0]} ${cen[1]}"
+            ./qsub_create_mixed_events.sh ${mult[$k]} ${mult[$inck]} ${vertex[$i]} ${vertex[$inci]} ${ep[$j]} ${ep[$incj]} ${cen[0]} ${cen[1]}
         done
     done
 done
