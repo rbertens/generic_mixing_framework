66a3e58dfefc8b6f286d5b27729b8d0afe282a23
rbertens <rbertens@cern.ch>
2017-08-30 15:24:37 +0200

    fix logic bug in simple evente selection class

--- AliGMFSimpleEventCuts.cxx
+++ AliGMFSimpleEventCuts.cxx
@@ -6,19 +6,34 @@
 ClassImp(AliGMFSimpleEventCuts);
 
 AliGMFSimpleEventCuts::AliGMFSimpleEventCuts() : TObject(),
+    fCheckMultiplicity(kFALSE),
     fMultiplicityMin(1), 
     fMultiplicityMax(-1),
+    fCheckVertex(kFALSE),
     fVertexMin(1),
     fVertexMax(-1),
+    fCheckEventPlane(kFALSE),
     fEventPlaneMin(1),
     fEventPlaneMax(-1),
+    fCheckCentrality(kFALSE),
     fCentralityMin(1),
     fCentralityMax(-1),
-    fRunNumberMin(SHRT_MIN),
-    fRunNumberMax(SHRT_MAX)
+    fCheckRunNumber(kFALSE),
+    fRunNumberMin(0),
+    fRunNumberMax(ULONG_MAX)
 {
   // default constructor
 }
+
+void AliGMFSimpleEventCuts::Diagnose() {
+    // check why an event doesn't pass the seleciton criteria
+    if(!fCurrentEvent) printf(" > No event found \n"); 
+    if(!PassesCentralitySelection()) printf(" Failed centrality check  \n"); 
+    if(!PassesVertexSelection()) printf(" Failed vertex check  \n");
+    if(!PassesEventPlaneSelection()) printf(" Failed event plane check  \n");
+    if(!PassesMultiplicitySelection()) printf(" Failed multiplicity check  \n");
+    if(!PassesRunNumberSelection()) printf(" Failed run number check  \n");
+}   
 
 Bool_t AliGMFSimpleEventCuts::IsSelected(AliGMFEventContainer* event) {
     // check event cuts
@@ -36,29 +51,34 @@ Bool_t AliGMFSimpleEventCuts::IsSelected(AliGMFEventContainer* event) {
 
 Bool_t AliGMFSimpleEventCuts::PassesCentralitySelection() {
     // check centrality criteria
+    if(!fCheckCentrality) return kTRUE;
     if(fCurrentEvent->GetCentrality() > fCentralityMin && fCurrentEvent->GetCentrality() < fCentralityMax) return kTRUE;
     return kFALSE;
 }
 
 Bool_t AliGMFSimpleEventCuts::PassesVertexSelection() {
     // check vertex criteria
+    if(!fCheckVertex) return kTRUE;
     if(fCurrentEvent->GetZvtx() > fVertexMin && fCurrentEvent->GetZvtx() < fVertexMax) return kTRUE;
     return kFALSE;
 }
 
 Bool_t AliGMFSimpleEventCuts::PassesEventPlaneSelection() {
     // check event plane criteria
+    if(!fCheckEventPlane) return kTRUE;
     if(fCurrentEvent->GetEventPlane() > fEventPlaneMin && fCurrentEvent->GetEventPlane() < fEventPlaneMax) return kTRUE;
     return kFALSE;
 }
 
 Bool_t AliGMFSimpleEventCuts::PassesMultiplicitySelection() {
     // check multiplicity criteria
+    if(!fCheckMultiplicity) return kTRUE;
     if(fCurrentEvent->GetMultiplicity() > fMultiplicityMin && fCurrentEvent->GetMultiplicity() < fMultiplicityMax) return kTRUE;
     return kFALSE;
 }
 
 Bool_t AliGMFSimpleEventCuts::PassesRunNumberSelection() {
+    if(!fCheckRunNumber) return kTRUE;
     if(fCurrentEvent->GetRunNumber() > fRunNumberMin && fCurrentEvent->GetRunNumber() < fRunNumberMax) return kTRUE;
     return kFALSE;
 }
--- AliGMFSimpleEventCuts.h
+++ AliGMFSimpleEventCuts.h
@@ -19,26 +19,33 @@ class AliGMFSimpleEventCuts : public TObject {
     Bool_t PassesEventPlaneSelection();
     Bool_t PassesRunNumberSelection();
 
+    void Diagnose();
+
     // setters
     void SetMultiplicityRange(Int_t min, Int_t max) {
+        fCheckMultiplicity = kTRUE;
         fMultiplicityMin = min;
         fMultiplicityMax = max;
     }
     void SetVertexRange(Float_t min, Float_t max) {
+        fCheckVertex = kTRUE;
         fVertexMin = min;
         fVertexMax = max;
     }
     void SetEventPlaneRange(Float_t min, Float_t max) {
+        fCheckEventPlane = kTRUE;
         fEventPlaneMin = min;
         fEventPlaneMax = max;
     }
 
     void SetCentralityRange(Float_t min, Float_t max) {
+        fCheckCentrality = kTRUE;
         fCentralityMin = min;
         fCentralityMax = max;
     }
     
     void SetRunNumberRange(ULong_t min, ULong_t max) {
+        fCheckRunNumber = kTRUE;
         fRunNumberMin = min;
         fRunNumberMax = max;
     }
@@ -46,14 +53,19 @@ class AliGMFSimpleEventCuts : public TObject {
  private:
 
     AliGMFEventContainer*  fCurrentEvent;  //! current event    
+    Bool_t      fCheckMultiplicity; // cut on multiplicity
     Int_t       fMultiplicityMin;   // minimum multiplicity
     Int_t       fMultiplicityMax;   // maximum multiplicity
+    Bool_t      fCheckVertex;       // cut on vertexz position
     Float_t     fVertexMin;         // minimum vertexz
     Float_t     fVertexMax;         // maximum vertexz
+    Bool_t      fCheckEventPlane;   // cut on event plane angle orientation
     Float_t     fEventPlaneMin;     // minimum event plane angle
     Float_t     fEventPlaneMax;     // maximum event plane angle
+    Bool_t      fCheckCentrality;   // cut on centrality percentile
     Float_t     fCentralityMin;     // minimum event centrality
     Float_t     fCentralityMax;     // maximum event centrality
+    Bool_t      fCheckRunNumber;    // cut on run number
     ULong_t     fRunNumberMin;      // minimum run number
     ULong_t     fRunNumberMax;      // maximum run number
 
