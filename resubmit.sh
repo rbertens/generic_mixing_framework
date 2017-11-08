8ea3371ebfbfc5f7e5b515eb9d6d53c3ef4ecab0
rbertens <rbertens@cern.ch>
2017-11-08 14:19:37 +0100

    minor tweak of submit scripts

--- resubmit.sh
+++ resubmit.sh
@@ -1,9 +1,13 @@
-for dir in */; do
-    if [ -f $dir/mixingQA.root ]; then
-           echo "job $dir was a success"
-       else
-           echo " !!!!!!!!!!!!! job $dir failed, resubmitting !!!!!!!!!!!!!!!!!!!!!!!" 
-           ls -l "$dir"
-           bsub -q 8nh $dir/create_mixed_events_autoscript.sh 
-       fi
-   done
+echo " -------------------------------- "
+echo "  welcome to the ME jet analyzer "
+echo "--------------------------------- "
+echo " "
+
+# create a unique folder
+cd SE_jets_$1_$2_$3
+if [ ! -f SE_jets_0_5.root ]; then
+    echo "i do not exist"
+    pwd
+    bsub -q 1nd SE_jet_analysis_autoscript.sh
+fi
+cd ..    
