f3d7c230920fb2117f802aec5661d5b7c3add867
rbertens <rbertens@cern.ch>
2017-11-17 14:26:50 +0100

    spring cleaning

--- compile.C
+++
@@ -1,8 +0,0 @@
-{
-    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFTTreeHeader.cxx+");
-    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFTTreeTrack.cxx+");
-    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFEventContainer.cxx+");
-    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFEventReader.cxx+");
-    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFHistogramManager.cxx+");
-    gROOT->LoadMacro("$PATH_TO_SOURCE/AliGMFMixingManager.cxx+");
-}
